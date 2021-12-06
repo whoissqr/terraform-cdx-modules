bd1afa6f57b22aa6ca907ad03f3d396e999fe16f
Asif Basha Mohammad <30190106+asifbasha-mohammad@users.noreply.github.com>
2021-12-07 03:49:47 +0530

    Add script to print terraform output for deployment

---
+++ aws/get-tf-outputs.sh
@@ -0,0 +1,9 @@
+
+echo "export CNC_CLUSTER_NAME=$(terraform output -raw cluster_name)"
+echo "export CNC_CLUSTER_REGION=$(terraform output -raw cluster_region)"
+echo "export CNC_PGHOST=$(terraform output -raw db_instance_address)"
+echo "export CNC_PGPORT=$(terraform output -raw db_instance_port)"
+echo "export CNC_PGUSER=$(terraform output -raw db_instance_username)"
+echo "export CNC_PGPASSWORD=$(terraform output -raw db_master_password)"
+echo "export CNC_S3_BUCKET_NAME=$(terraform output -raw s3_bucket_name)"
+echo "export CNC_S3_BUCKET_REGION=$(terraform output -raw s3_bucket_region)"
